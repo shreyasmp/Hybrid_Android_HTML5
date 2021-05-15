ecf46e3bbdbd91c9dca960bf7b4ca8628a2c02d0
Shreyas Muthkur <shreyasspidy@gmail.com>
2021-05-15 15:51:42 -0500

    Fixed for studio 4.2.1 compatibility

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,24 +1,17 @@
 object Versions {
 
-    const val gradle = "4.1.3"
+    const val gradle = "4.2.1"
     const val kotlin = "1.4.21"
     const val kotlinCoroutine = "1.4.0"
 
-    const val coreKTX = "1.3.2"
     const val appCompat = "1.2.0"
     const val constraintLayout = "2.0.4"
     const val fragmentKTX = "1.3.2"
     const val material = "1.1.0"
 
-    const val archLifeCycle = "2.2.0"
-    const val retrofit = "2.9.0"
-    const val recyclerView = "1.1.0"
-    const val cardView = "1.0.0"
     const val support = "1.0.0"
     const val dagger = "2.28.3"
-    const val okHttp3 = "4.4.0"
 
-    const val androidxTest = "1.2.0"
     const val jUnit = "4.12"
     const val extJUnit = "1.1.2"
     const val espresso = "3.3.0"
@@ -30,7 +23,5 @@ object Versions {
 
     const val robolectric = "4.5.1"
     const val robolectricShadows = "4.5.1"
-    const val kotlinCoroutineTest = "1.4.2"
     const val nhaarmanMockito = "2.2.0"
-    const val okHttpMockServer = "4.7.2"
 }
